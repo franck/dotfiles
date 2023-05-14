2740b5855379abcf5eb7476582b7af3602e61b0a
Franck D'agostini <franck.dagostini@gmail.com>
2023-05-14 11:44:37 +0200

    remove unecessary language servers

--- nvim/init.lua
+++ nvim/init.lua
@@ -405,7 +405,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls' }
+local servers = { 'pyright', 'tsserver' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
